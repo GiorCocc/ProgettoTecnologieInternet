9c4bf44b4c12464cf71eca31a33bd3928c107460
sirMallet <104863444+sirMallet@users.noreply.github.com>
2023-04-25 12:43:10 +0200

    Add files via upload

---
+++ src/lib/game/entities/kill.js
@@ -0,0 +1,37 @@
+ig.module(
+    'game.entities.kill'
+)
+.requires(
+    'impact.entity'
+)
+.defines(function() {
+
+EntityKill = ig.Entity.extend({
+    _wmDrawBox: true,
+    _wmBoxColor: 'rgba(255, 0, 0, 0.7)',
+    _wmScalable: true,
+
+    message : 'You died!',
+    killTimer: null,
+    font:  new ig.Font( 'media/04b03.font.png' ),
+    type: ig.Entity.TYPE.NONE,
+    checkAgainst: ig.Entity.TYPE.BOTH,
+    
+    check: function( other ) {
+        other.kill();
+        this.killTimer = new ig.Timer( 2 );
+        
+    },
+    update: function() {},
+    draw: function() {
+        
+        if( this.killTimer && this.killTimer.delta()<  0 ) { // if the timer is set and has not expired, delta significa che è passato un secondo
+            this.font.draw( this.message, ig.system.width/2, ig.system.height/2 );
+        }
+        else{
+            this.font.draw( "", ig.system.width/2, ig.system.height/2 );
+        };
+
+    }
+});
+});
