03be3c0a65615c14edcbc2131bf6e975e071ff1d
giorcocc <giorcocc@gmail.com>
2023-04-27 21:13:29 +0200

    added rotation animation

--- src/lib/game/entities/weapon.js
+++ src/lib/game/entities/weapon.js
@@ -55,16 +55,56 @@ ig.module(
             // TODO: ruotare l'arma in senso circolare, di 45 gradi ogni volta che il giocatore preme il tasto per attaccare
             if (ig.input.pressed('attack')) {
                 if(this.pos.x < player.pos.x){
-                    this.currentAnim = new ig.Animation(this.animSheet, 0.1, [11,10,9,8, 9,10, 0], true);
-                    
+                    this.rotationAnimation('left', [11,10,9,8, 9,10, 0]);
                 }  
                 else{
-                    this.currentAnim = new ig.Animation(this.animSheet, 0.1, [7,6,5,4, 5,6, 0], true);
+                    this.rotationAnimation('right', [7,6,5,4, 5,6, 0]);
                 }
                     
             }
             
 
+            
+        },
+
+        rotationAnimation: function(side, frames) {
+            var speed = 0.1;
+            var rotationSpeed = 45;
+            var direction = 1;
+
+            if (side === 'left') {
+                direction = -1;
+            } else {
+                direction = 1;
+            }
+
+            var self = this;
+            var i = 0;
+
+            // while the animation is running, the weapon rotates
+            var interval = setInterval(function() {
+                self.currentAnim = new ig.Animation(self.animSheet, speed, [frames[i]], true);
+                self.currentAnim.angle += direction * rotationSpeed;
+                i++;
+                if (i === frames.length) {
+                    clearInterval(interval);
+                    self.finishAttack(side);
+                }
+            } , speed * 700);
+        },
+
+
+        resetAnimation: function() {
+            this.currentAnim = new ig.Animation(this.animSheet, 0.1, [0], true);
+
+        },
+
+        finishAttack: function(direction) {
+            var self = this;
+            setTimeout(function() {
+                self.resetAnimation();
+                self.hasPlayerAttacked = false;
+            }, 500);
             
         },
 
