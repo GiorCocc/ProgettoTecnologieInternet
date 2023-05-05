0839fc61d5c75ad80396a5202caecc48d9077597
giorcocc <giorcocc@gmail.com>
2023-05-05 21:49:13 +0200

    fixed

--- signalling/server.js
+++ signalling/server.js
@@ -2,7 +2,6 @@ var PORT = 8034;
 var MAX_ROOM_USERS = 5;
 
 var fs = require('fs');
-var log = console.log.bind(console);
 var io = require('socket.io')(PORT);
 
 var rooms = {};
@@ -10,35 +9,30 @@ var lastUserId = 0;
 var lastRoomId = 0;
 
 var MessageType = {
-  // A messages you send to server, when want to join or leave etc.
   JOIN: 'join',
   DISCONNECT: 'disconnect',
 
-  // You receive room info as a response for join command. It contains information about
-  // the room you joined, and it's users
+  // Room related
   ROOM: 'room',
 
-  // A messages you receive from server when another user want to join or leave etc.
+  // User related
   USER_JOIN: 'user_join',
   USER_READY: 'user_ready',
   USER_LEAVE: 'user_leave',
 
-  // WebRtc signalling info, session and ice-framework related
+  // Communication related
   SDP: 'sdp',
   ICE_CANDIDATE: 'ice_candidate',
 
-  // Errors... shit happens
+  // Error related
   ERROR_ROOM_IS_FULL: 'error_room_is_full',
   ERROR_USER_INITIALIZED: 'error_user_initialized'
 };
 
-function User() {
-  this.userId = ++lastUserId;
-}
+function User() { this.userId = ++lastUserId; }
+
 User.prototype = {
-  getId: function() {
-    return this.userId;
-  }
+  getId: function() { return this.userId; }
 };
 
 function Room(name) {
@@ -46,41 +40,47 @@ function Room(name) {
   this.users = [];
   this.sockets = {};
 }
+
 Room.prototype = {
   getName: function() {
     return this.roomName;
   },
+
   getUsers: function() {
     return this.users;
   },
+
   getUserById: function(id) {
-    return this.users.find(function(user) {
-      return user.getId() === id;
-    });
+    return this.users.find(function(user) { return user.getId() === id; });
   },
+
   numUsers: function() {
     return this.users.length;
   },
+
   isEmpty: function() {
     return this.users.length === 0;
   },
+
   addUser: function(user, socket) {
     this.users.push(user);
     this.sockets[user.getId()] = socket;
   },
+
   removeUser: function(id) {
-    this.users = this.users.filter(function(user) {
-      return user.getId() !== id;
-    });
+    this.users = this.users.filter(function(user) { return user.getId() !== id; });
     delete this.sockets[id];
   },
+
   sendTo: function(user, message, data) {
     var socket = this.sockets[user.getId()];
     socket.emit(message, data);
   },
+
   sendToId: function(userId, message, data) {
     return this.sendTo(this.getUserById(userId), message, data);
   },
+
   broadcastFrom: function(fromUser, message, data) {
     this.users.forEach(function(user) {
       if (user.getId() !== fromUser.getId()) {
@@ -124,41 +124,48 @@ function handleSocket(socket) {
       roomName: room.getName(),
       users: room.getUsers()
     });
+
     // Notify others of a new user joined
     room.broadcastFrom(user, MessageType.USER_JOIN, {
       userId: user.getId(),
       users: room.getUsers()
     });
-    log('User %s joined room %s. Users in room: %d',
-      user.getId(), room.getName(), room.numUsers());
+
+    console.log('User %s joined room %s. Users in room: %d', user.getId(), room.getName(), room.numUsers());
   }
 
   function getOrCreateRoom(name) {
     var room;
+
+    // if the room name is not specified, create a new one
     if (!name) {
       name =  ++lastRoomId + '_room';
     }
+
+    // if the room does not exist, create a new one
     if (!rooms[name]) {
       room = new Room(name);
       rooms[name] = room;
     }
+
     return rooms[name];
   }
 
   function onLeave() {
-    if (room === null) {
-      return;
-    }
-    room.removeUser(user.getId());
-    log('User %d left room %s. Users in room: %d',
-      user.getId(), room.getName(), room.numUsers());
+    if (room === null) return;
+
+    room.removeUser(user.getId());  // remove user from room
+
+    console.log('User %d left room %s. Users in room: %d', user.getId(), room.getName(), room.numUsers());
+
+    // if there are no users left, the room is no longer needed
     if (room.isEmpty()) {
-      log('Room is empty - dropping room %s', room.getName());
+      console.log('Room is empty - dropping room %s', room.getName());
       delete rooms[room.getName()];
     }
-    room.broadcastFrom(user, MessageType.USER_LEAVE, {
-      userId: user.getId()
-    });
+
+    // notify others of a user left
+    room.broadcastFrom(user, MessageType.USER_LEAVE, { userId: user.getId() });
   }
 
   function onSdp(message) {
@@ -177,4 +184,4 @@ function handleSocket(socket) {
 }
 
 io.on('connection', handleSocket);
-log('Running room server on port %d', PORT);
+console.log('Running room server on port %d', PORT);
